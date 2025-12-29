59db95140e6e647c96e19604ac5d4939764deed9
Jayaprakash8887 <njayaprakash.8887@gmail.com>
2025-12-29 13:25:59 +0530

    favicon edit

--- artifacts/ui-design/src/pages/NewClaim.tsx
+++
@@ -1,8 +0,0 @@
-import { useNavigate } from 'react-router-dom';
-import { ClaimSubmissionForm } from '@/components/claims/ClaimSubmissionForm';
-
-export default function NewClaim() {
-  const navigate = useNavigate();
-
-  return <ClaimSubmissionForm onClose={() => navigate('/claims')} />;
-}
