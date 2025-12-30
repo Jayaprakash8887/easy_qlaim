dc75102d7428e61729e08702a366c7b18b81f796
jayaprakashnarayanaswamy <jayaprakash.n@tarento.com>
2025-12-30 13:01:37 +0530

    padding space reduction

--- frontend/src/components/layout/AppLayout.tsx
+++ frontend/src/components/layout/AppLayout.tsx
@@ -38,7 +38,7 @@ export function AppLayout() {
           !isMobile && (sidebarCollapsed ? 'lg:pl-16' : 'lg:pl-64')
         )}
       >
-        <div className="container mx-auto p-6">
+        <div className="container mx-auto px-6 py-4">
           <Breadcrumbs />
           <Outlet />
         </div>
